c85d7732a87458c4e04c62de0a069e589dd60d7c
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-03-19 11:33:32 -0700

    Including Try/Except and Raise

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 def select_dish(foods, selected_food):
+    if selected_food < 0 or selected_food >= len(foods):
+        raise IndexError("Out of bound")
     print(f"Ah, {foods[selected_food]}! An excellent choice!")
+    print("yum!")
+
 
 def your_menu(foods):
-    try:
-        index = 1
-        for dish in foods:
-            print(f"{index}. {dish}")
-            index += 1
+    while True:
+        try:
+            index = 1
+            for dish in foods:
+                print(f"{index}. {dish}")
+                index += 1
     
-        selected_choice = int(input("Your order number? "))
-        select_dish(foods, selected_choice - 1)
-    except IndexError as error:
-        print(f"{error} was entered.")
-        print("Next time try entering something on the menu!")
+            selected_choice = int(input("Your order number? "))
+            select_dish(foods, selected_choice - 1)
+            break
+        
+        except IndexError as error:
+            print(f"{error} was entered.")
+            print("Next time try entering something on the menu!")
+
+        except ValueError as error:
+            print(f"{error} was entered.")
+            print("Next time try entering an integer")
+            print("------------------")
 
 menu_items = [
     "Yakisoba",
@@ -23,4 +35,3 @@ menu_items = [
 ]
 
 your_menu(menu_items)
-print("Yum!")
